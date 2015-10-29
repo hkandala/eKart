c58292ab1608de88de07c3018ae7a9e0d8d840d9
Harish Kandala <kandalaharish95@gmail.com>
2015-10-29 08:16:45 +0530

    Database Files

--- eKart.sql
+++ eKart.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 22, 2015 at 06:17 AM
+-- Generation Time: Oct 29, 2015 at 08:12 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -27,11 +27,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `address` (
+  `id` int(10) NOT NULL,
   `userid` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `mobile` int(15) NOT NULL,
+  `mobile` varchar(10) NOT NULL,
   `address` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `address`
+--
+
+INSERT INTO `address` (`id`, `userid`, `name`, `mobile`, `address`) VALUES
+(1, 1, 'College', '9790995811', 'L - 414, VIT Hostels, VIT University, Vellore'),
+(2, 1, 'Home', '9790995811', 'Flat No: 102, Sai Nikitha Residency, Madhura Nagar, Khadi Colony, Tirupati'),
+(3, 3, 'Home', '9866177925', 'Flat no: 352, Venkat Apartments, Gandhi Nagar, Hyderabad');
 
 -- --------------------------------------------------------
 
@@ -51,7 +61,12 @@ CREATE TABLE IF NOT EXISTS `cart` (
 
 INSERT INTO `cart` (`userid`, `productid`, `qty`) VALUES
 (1, 8, 1),
-(1, 19, 1);
+(1, 19, 1),
+(2, 5, 1),
+(2, 16, 1),
+(3, 33, 1),
+(4, 3, 1),
+(5, 21, 1);
 
 -- --------------------------------------------------------
 
@@ -119,9 +134,9 @@ INSERT INTO `products` (`id`, `catid`, `name`, `price`, `instock`, `description`
 (18, 2, 'Dell Inspiron 3148 (Intel 2-in-1 Laptop) (4th Gen Ci3/ 4GB/ 500GB/ Win8.1/ Touch) (314834500iST)', 36197, 1, 'Key Features of Dell Inspiron 3148 (Intel 2-in-1 Laptop) (4th Gen Ci3/ 4GB/ 500GB/ Win8.1/ Touch) (314834500iST)\r\nIntel Core i3 (4th Gen)\r\n4 GB DDR3 RAM\r\n500 GB HDD\r\nWindows 8.1', '2015-10-17 18:35:02'),
 (19, 2, 'Apple MD101HN/A Macbook Pro MD101HN/A Intel Core i5 - (4 GB DDR3/500 GB HDD/Mac OS)', 55990, 1, 'Key Features of Apple MD101HN/A Macbook Pro MD101HN/A Intel Core i5 - (4 GB DDR3/500 GB HDD/Mac OS)\r\nIntel Core i5\r\n4 GB DDR3 RAM\r\n500 GB HDD\r\nOS X Mavericks', '2015-10-17 18:35:56'),
 (20, 2, 'Apple MJVE2HN/A Ultrabook (Core i5 5th Gen/ 4GB/ 128GB/ Mac OS X Yosemite)', 59990, 1, 'Key Features of Apple MJVE2HN/A Ultrabook (Core i5 5th Gen/ 4GB/ 128GB/ Mac OS X Yosemite)\r\nIntel Core i5\r\n4 GB DDR3 RAM\r\n128 GB SSD\r\nMac OS X Yosemite', '2015-10-17 18:36:41'),
-(21, 3, 'Suspense Men''s Solid Casual Shirt', 449, 1, 'GENERAL DETAILS\nPattern	        Solid\nOccasion	Casual\nIdeal For	Men''s', '2015-10-17 18:43:30'),
-(22, 3, 'Suspense Men''s Solid Casual Shirt', 449, 1, 'GENERAL DETAILS\r\nPattern	        Solid\r\nIdeal For	Men''s\r\nOccasion	Casual', '2015-10-17 18:44:54'),
-(23, 3, 'Suspense Men''s Solid Casual Shirt', 449, 1, 'GENERAL DETAILS\r\nPattern	        Solid\r\nIdeal For	Men''s\r\nOccasion	Casual', '2015-10-17 18:50:07'),
+(21, 3, 'Suspense Men''s Solid Casual Shirt', 449, 1, 'GENERAL DETAILS\nPattern	        Solid\nOccasion	Casual\nIdeal For         Men''s', '2015-10-17 18:43:30'),
+(22, 3, 'Suspense Men''s Solid Casual Shirt', 449, 1, 'GENERAL DETAILS\nPattern	        Solid\nIdeal For         Men''s\nOccasion	Casual', '2015-10-17 18:44:54'),
+(23, 3, 'Suspense Men''s Solid Casual Shirt', 449, 1, 'GENERAL DETAILS\nPattern	        Solid\nIdeal For         Men''s\nOccasion	Casual', '2015-10-17 18:50:07'),
 (24, 3, 'Top Notch Solid Men''s Henley T-Shirt', 669, 0, 'Top Notch Solid Men''s Henley T-Shirt (Pack of 2) Price: Rs. 669\r\nBasics never wear out of fashion. You can team your basics with literally everything. They can carry any look with ease. Pair it with a blazer and a jean to give it that not-so-formal yet casual look, with a pair of 3/4ths or chinos and uber cool loafer it gives you fun outing look. The premium knits T-shirts from Top Notch are made of 100% combed cotton. Pre-shrunk to let you worry less about shrinkage. They are highly comfortable and smooth on skin. Top Notch makes it easy on your pocket by letting you buy 2 for the price of 1.', '2015-10-17 18:51:00'),
 (25, 3, 'Ben Carter Slim Fit Men''s Jeans', 525, 1, 'Key Features of Ben Carter Slim Fit Men''s Jeans\r\nInseam: 33.5 inch\r\nRise: Mid Rise\r\nColor: Black\r\nFit: Slim', '2015-10-17 18:51:41'),
 (26, 3, 'Ishin Printed Fashion Georgette Sari', 429, 0, 'Key Features of Ishin Printed Fashion Georgette Sari\r\nWith Blouse Piece\r\nFashion Sari\r\nGeorgette\r\nPink', '2015-10-17 18:52:14'),
@@ -149,8 +164,21 @@ INSERT INTO `products` (`id`, `catid`, `name`, `price`, `instock`, `description`
 CREATE TABLE IF NOT EXISTS `purchase` (
   `id` int(10) NOT NULL,
   `userid` int(10) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `addressid` int(10) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `purchase`
+--
+
+INSERT INTO `purchase` (`id`, `userid`, `addressid`, `timestamp`) VALUES
+(10, 1, 1, '2015-10-22 22:29:55'),
+(11, 1, 2, '2015-10-22 22:31:03'),
+(12, 3, 3, '2015-10-22 23:07:25'),
+(13, 1, 1, '2015-10-29 02:31:48'),
+(14, 1, 1, '2015-10-29 02:32:52'),
+(15, 1, 1, '2015-10-29 02:36:19');
 
 -- --------------------------------------------------------
 
@@ -164,15 +192,18 @@ CREATE TABLE IF NOT EXISTS `review` (
   `productid` int(10) NOT NULL,
   `rating` int(11) NOT NULL,
   `comment` varchar(1000) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `review`
 --
 
 INSERT INTO `review` (`id`, `userid`, `productid`, `rating`, `comment`) VALUES
+(11, 1, 2, 4, 'Really worth for the price!!'),
 (3, 1, 4, 4, 'Very fast delivery..Love it'),
-(2, 1, 8, 5, 'Excellent Product'),
+(2, 1, 8, 5, 'Excellent Product !!'),
+(7, 1, 15, 3, 'Disappointed. Product is not as expected'),
+(9, 1, 16, 5, 'Great product!'),
 (4, 1, 19, 4, 'Love Apple products.'),
 (1, 1, 26, 3, 'Good product'),
 (5, 2, 8, 4, 'Love this phone!!'),
@@ -190,6 +221,22 @@ CREATE TABLE IF NOT EXISTS `solditems` (
   `qty` int(5) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `solditems`
+--
+
+INSERT INTO `solditems` (`purchaseid`, `productid`, `qty`) VALUES
+(10, 8, 1),
+(10, 19, 1),
+(11, 12, 1),
+(12, 15, 1),
+(12, 24, 1),
+(12, 30, 3),
+(13, 8, 1),
+(13, 19, 1),
+(14, 21, 1),
+(15, 31, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -204,7 +251,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(100) NOT NULL,
   `mobile` bigint(10) DEFAULT NULL,
   `dob` date DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -215,7 +262,8 @@ INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `mobile`, `dob
 (2, 'Goutham', 'Reddy', 'goutamkreddy@gmail.com', '$2y$10$IU1VAAMnUKQ0zicYNmw00uqJknyGNHuT/4W4nehnkBeJwiFXldgYG', 9908862490, '1995-08-25'),
 (3, 'Chandra Prakash', 'Reddy', 'cpreddy@gmail.com', '$2y$10$IU1VAAMnUKQ0zicYNmw00uqJknyGNHuT/4W4nehnkBeJwiFXldgYG', 9866177925, '1995-03-05'),
 (4, 'Suhas', 'Tejaskanda', 'suhastejas@gmail.com', '$2y$10$IU1VAAMnUKQ0zicYNmw00uqJknyGNHuT/4W4nehnkBeJwiFXldgYG', 9866175683, '1995-10-30'),
-(5, 'Dhanushik', 'Macharla', 'dhanushikmacharla@gmail.com', '$2y$10$IU1VAAMnUKQ0zicYNmw00uqJknyGNHuT/4W4nehnkBeJwiFXldgYG', 9793758552, '1996-01-07');
+(5, 'Dhanushik', 'Macharla', 'dhanushikmacharla@gmail.com', '$2y$10$IU1VAAMnUKQ0zicYNmw00uqJknyGNHuT/4W4nehnkBeJwiFXldgYG', 9793758552, '1996-01-07'),
+(6, 'Manoj', 'Kumar', 'kmkmanoj1991@gmail.com', '$2y$10$Fac8mGrZqshwM4dsJQMIme6toMwCnwhzhww.Gyi1IwBK0KsEeJ5rC', 9989833608, '1991-10-12');
 
 --
 -- Indexes for dumped tables
@@ -225,7 +273,7 @@ INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `mobile`, `dob
 -- Indexes for table `address`
 --
 ALTER TABLE `address`
-  ADD KEY `userid` (`userid`);
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `userid_2` (`userid`,`name`), ADD UNIQUE KEY `userid_3` (`userid`,`name`), ADD KEY `userid` (`userid`);
 
 --
 -- Indexes for table `cart`
@@ -249,7 +297,7 @@ ALTER TABLE `products`
 -- Indexes for table `purchase`
 --
 ALTER TABLE `purchase`
-  ADD PRIMARY KEY (`id`), ADD KEY `userid` (`userid`);
+  ADD PRIMARY KEY (`id`), ADD KEY `userid` (`userid`), ADD KEY `addressid` (`addressid`);
 
 --
 -- Indexes for table `review`
@@ -274,6 +322,11 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
@@ -287,17 +340,17 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `purchase`
 --
 ALTER TABLE `purchase`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- Constraints for dumped tables
 --
@@ -325,6 +378,7 @@ ADD CONSTRAINT `products_catid` FOREIGN KEY (`catid`) REFERENCES `categories` (`
 -- Constraints for table `purchase`
 --
 ALTER TABLE `purchase`
+ADD CONSTRAINT `purchase_addressid` FOREIGN KEY (`addressid`) REFERENCES `address` (`id`) ON DELETE CASCADE,
 ADD CONSTRAINT `purchase_userid` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
